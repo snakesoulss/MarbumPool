33e8d286e7ef587a9e0dab16a3c72c6b1e0d9d97
snakesoulss <105731802+snakesoulss@users.noreply.github.com>
2022-12-03 23:52:26 +0300

    activitys

---
+++ app/src/main/java/pool/marbum/marbumpoll/Conflict.java
@@ -0,0 +1,14 @@
+package pool.marbum.marbumpoll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Conflict extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conflict);
+    }
+}
---
+++ app/src/main/java/pool/marbum/marbumpoll/RulesActivity.java
@@ -0,0 +1,45 @@
+package pool.marbum.marbumpoll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RulesActivity extends AppCompatActivity {
+
+    Button generalInfo;
+    Button admin;
+    Button conflict;
+    Button prohibition;
+    Button visitors;
+    Button visitorsCan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+
+        generalInfo = findViewById(R.id.general_info);
+        generalInfo.setOnClickListener(view -> {
+            Intent intent = new Intent(RulesActivity.this, GeneralInfo.class);
+            startActivity(intent);
+        });
+
+        admin = findViewById(R.id.admin);
+        admin.setOnClickListener(view -> {
+            Intent intent = new Intent(RulesActivity.this, Admin.class);
+            startActivity(intent);
+        });
+
+
+        conflict = findViewById(R.id.conflict);
+        conflict.setOnClickListener(view -> {
+            Intent intent = new Intent(RulesActivity.this, Conflict.class);
+            startActivity(intent);
+        });
+
+
+    }
+}
