a4590c9ec93213a44eb2335369d181920dda5d65
snakesoulss <105731802+snakesoulss@users.noreply.github.com>
2023-01-05 03:13:25 +0300

    registr

--- app/src/main/java/pool/marbum/marbumpoll/MainActivity.java
+++ app/src/main/java/pool/marbum/marbumpoll/MainActivity.java
@@ -1,20 +1,46 @@
 package pool.marbum.marbumpoll;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
     Button price;
     Button rules;
+    FloatingActionButton account;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        account = findViewById(R.id.account);
 
+        account.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, Account.class);
+            startActivity(intent);
+        });
+
+
+        boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                .getBoolean("isFirstRun", true);
+
+        if (isFirstRun) {
+
+            startActivity(new Intent(MainActivity.this, Registration.class));
+            Toast.makeText(MainActivity.this, "First Run", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+
+        getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit()
+                .putBoolean("isFirstRun", false).apply();
         price = findViewById(R.id.button_price);
         price.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, PriceActivity.class);
--- app/src/main/java/pool/marbum/marbumpoll/PriceActivity.java
+++ app/src/main/java/pool/marbum/marbumpoll/PriceActivity.java
@@ -1,17 +1,16 @@
 package pool.marbum.marbumpoll;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PriceActivity extends AppCompatActivity {
 
-    Button back;
-    Button childTicket;
-    Button Ticket;
+    FloatingActionButton back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
